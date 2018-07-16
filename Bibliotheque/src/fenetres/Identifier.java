36fe769b4e5065e27672ce0946e1a28b855a9520
eleves <eleves@10.2.20.14>
2018-07-16 10:26:20 -0400

    J'ai arrete sur livre

--- Bibliotheque/src/fenetres/Identifier.java
+++ Bibliotheque/src/fenetres/Identifier.java
@@ -1,13 +1,17 @@
 
 package fenetres;
 
-import java.awt.Point;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Rectangle;
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 /**
  *
@@ -20,16 +24,16 @@ public class Identifier extends JFrame implements InterfaceFenetres{
     private final String TITRE_PANE = "Bibliothèque Marguerite Bourgeoys";
     private final String UTILISATEUR = "Utilisateur";
     private final String MOT_DE_PASSE = "Mot de Passe";
-    private final int X = 100;
-    private final int Y = 100;
-    private final int WIDTH = 700;
-    private final int HEIGHT = 500;
+    private final int TX_WITH = 100;
+    private final int TX_HEIGHT = 30;
+    private final int WIDTH = 600;
+    private final int HEIGHT = 400;
     
     private final int BDR = 5;
     
     // Objects
     private MenuBarPrincipal menuBar;
-    private JPanel panel;
+    private JPanel panel, userPanel, motPassePanel;
     private JLabel labelUtilisateur, labelMotdePasse, labelMarguerite;
     private JTextField textUtilisateur;
     private JPasswordField passwordField;
@@ -43,6 +47,7 @@ public class Identifier extends JFrame implements InterfaceFenetres{
 
     public Identifier() {
         initialiser();
+        definirLayout();
     }
     
     @Override
@@ -50,11 +55,15 @@ public class Identifier extends JFrame implements InterfaceFenetres{
         // Préférences
         Identifier.courant = this;
         setTitle(TITRE);
-        setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Fenêtre
+        setResizable(false);
         setBounds(new Rectangle(WIDTH, HEIGHT));
-        // Fonctionne près définir la taille
-        setLocationRelativeTo(null);
+        setLocationRelativeTo(null); /* Fonctionne près définir la taille */
+        //
+        panel = new JPanel();
+        panel.setBorder(new EmptyBorder(BDR, BDR, BDR, BDR));
+        panel.setLayout(new BoxLayout(panel,BoxLayout.PAGE_AXIS));
         // Menus
         menuBar = new MenuBarPrincipal();
         this.setJMenuBar(menuBar);
@@ -62,10 +71,27 @@ public class Identifier extends JFrame implements InterfaceFenetres{
         labelMarguerite = new JLabel(TITRE_PANE);
         labelMotdePasse = new JLabel(MOT_DE_PASSE);
         labelUtilisateur = new JLabel(UTILISATEUR);
+        
+        textUtilisateur = new JTextField();
+        textUtilisateur.setColumns(15);
+        textUtilisateur.setToolTipText(UTILISATEUR);
+        
+        passwordField = new JPasswordField();
+        passwordField.setColumns(15);
+        //passwordField.setToolTipText(MOT_DE_PASSE);
+        
+        panel.add(labelMarguerite);
+        panel.add(labelUtilisateur);
+        panel.add(textUtilisateur);
+        panel.add(labelMotdePasse);
+        panel.add(passwordField);
+        
+        this.setContentPane(panel);
     }
 
     @Override
     public void definirLayout() {
+        
         
     }
     
