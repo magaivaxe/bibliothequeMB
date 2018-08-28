520d6d0f9e78b5910c26845d9d010f2d56e74812
Magaivaxe <mpg.salgad@gmail.com>
2018-08-27 23:10:53 -0400

    A propos listener ok

--- Bibliotheque/src/framesListeners/AProposListener.java
+++ Bibliotheque/src/framesListeners/AProposListener.java
@@ -1,22 +1,32 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 
  */
 package framesListeners;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author sire_marcos
  */
 public class AProposListener implements ActionListener{
-
+    //Fields
+    final String TITLE = "Bibliothèque Marguerite Bourgeoys";
+    final String PHONE = "514 999 9999";
+    final String OPEN = "8h le matin";
+    final String CLOSE = "17h l'après midi";
+    final String MAIL = "margherite@mb.ca";
+    final String MSG = "Heure d'ouverture: "+ OPEN +"\n"
+                     + "Heure de fermeture: "+ CLOSE +"\n"
+                     + "Téléphone: " + PHONE + "\n"
+                     + "Courriel: " + MAIL;
+    
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+        //
+        JOptionPane.showMessageDialog(
+            null, MSG, TITLE, JOptionPane.INFORMATION_MESSAGE);
     }
-    
 }
